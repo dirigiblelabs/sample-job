ffc6b398a8eec0d1cbf247f5e419bf6c0093267f
delchev <delchevn@gmail.com>
2022-06-20 15:09:23 +0300

    initial

---
+++ sample-job/myhandler.js
@@ -0,0 +1,23 @@
+var scheduler = require("job/v4/scheduler");
+
+let job = scheduler.getJob("/sample-job/myjob.job");
+
+let stringParam = job.getParameter("stringParam");
+let numberParam = job.getParameter("numberParam");
+let boolParam = job.getParameter("boolParam");
+let choiceParam = job.getParameter("choiceParam");
+let notExisting = job.getParameter("notExisting");
+
+console.log('echo stringParam: ' + stringParam);
+console.log('echo numberParam: ' + numberParam);
+console.log('echo boolParam: ' + boolParam);
+console.log('echo choiceParam: ' + choiceParam);
+console.log('echo notExisting: ' + notExisting);
+
+// enable Job
+//job.enable();
+
+// disable Job
+//job.disable();
+
+//job.trigger({ "stringParam": "programmatic value" });
